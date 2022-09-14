0485c4ea7d43930da4472b0cd8af88b1e3afc6aa
Marcos Vinícius Cavalcante Oliveira <mvinic2011@gmail.com>
2022-09-14 18:37:53 -0300

    Utilizando o Map (chave, valor)

---
+++ src/main/kotlin/InteratingOverAMap.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+// Imprimir as representações binárias dos caracteres de A a F
+
+fun main() {
+    val binaryRepresentation = TreeMap<Char, String>()
+
+    for(c in 'A'..'F') {
+        val binary = Integer.toBinaryString(c.toInt())
+        binaryRepresentation[c] = binary
+    }
+
+    for((letter, binary) in binaryRepresentation) {
+        println("$letter - $binary")
+    }
+}
